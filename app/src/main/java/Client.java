c29c066f7444c273d4cdf3fb37c3dbb187775ccb
advait <deshpandeadvait88@gmail.com>
2016-09-01 21:01:54 +0530

    first commit

---
+++ app/src/main/java/Client.java
@@ -0,0 +1,47 @@
+import java.net.*;
+import java.io.*;
+
+/**
+ * Created by advait on 1/9/16.
+ */
+public class Client {
+    String input="";
+    String content="";
+    void getConnection()
+    {
+        try{
+
+            BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+            Socket so = new Socket("192.168.0.104",1294);
+            InputStream ips = so.getInputStream();
+            DataInputStream dis = new DataInputStream(ips);
+            OutputStream os = so.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            System.out.println("Enter Input: ");
+
+            while(true)
+            {
+                input = br.readLine();
+                dos.writeUTF(input);
+
+                content = dis.readUTF();
+                if(content.equals("END")||content.equals("end"))
+                {
+                    so.close();
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Client obj = new Client();
+        obj.getConnection();
+    }
+
+
+}
